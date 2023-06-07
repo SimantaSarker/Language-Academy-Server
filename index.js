8edb7bf4bd793c3a693a7b8088bb39955ea3d193
SimantaSarker <simantasarker0000@gmail.com>
2023-06-08 00:44:52 +0600

    admin can update user status

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-
 // middle ware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sk94onm.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,40 +25,64 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const usersCollection=client.db("languageDb").collection("users")
+    const usersCollection = client.db("languageDb").collection("users");
 
-
-    app.post("/users",async(req,res)=>{
-      const user=req.body;
-      const query={email:user.email};
-      const alreadyExistsUser=await usersCollection.findOne(query);
-      if(alreadyExistsUser)
-      {
-        return res.send({message:"You are login or signup already with your email"})
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const alreadyExistsUser = await usersCollection.findOne(query);
+      if (alreadyExistsUser) {
+        return res.send({
+          message: "You are login or signup already with your email",
+        });
       }
-      const result=await usersCollection.insertOne(user);
-      res.send(result)
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-    })
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      if (body.adminId == 1) {
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      };
 
-
-
+      if(body.instructorsId == 2)
+      {
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "instructors",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Server is running");
